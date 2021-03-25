a91d3f7f58c59dc749bdbda8dbcbd0987245f6fa
AnttiHalmetoja <anttihalmetoja@gmail.com>
2021-03-25 14:24:40 +0200

    week 2 injection exes done

---
+++ viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kauppa.java
@@ -0,0 +1,50 @@
+package ohtu.verkkokauppa;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Kauppa {
+
+    private VarastoIo varasto;
+    private PankkiIo pankki;
+    private Ostoskori ostoskori;
+    private ViitegeneraattoriIo viitegeneraattori;
+    private String kaupanTili;
+    @Autowired
+    public Kauppa(VarastoIo varasto, PankkiIo pankki, ViitegeneraattoriIo viitegeneraattori) {
+        this.varasto = varasto;
+        this.pankki = pankki;
+        this.viitegeneraattori = viitegeneraattori;
+        kaupanTili = "33333-44455";
+    }
+
+    public Kauppa() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void aloitaAsiointi() {
+        ostoskori = new Ostoskori();
+    }
+
+    public void poistaKorista(int id) {
+        Tuote t = varasto.haeTuote(id); 
+        varasto.palautaVarastoon(t);
+    }
+
+    public void lisaaKoriin(int id) {
+        if (varasto.saldo(id)>0) {
+            Tuote t = varasto.haeTuote(id);             
+            ostoskori.lisaa(t);
+            varasto.otaVarastosta(t);
+        }
+    }
+
+    public boolean tilimaksu(String nimi, String tiliNumero) {
+        int viite = viitegeneraattori.uusi();
+        int summa = ostoskori.hinta();
+        
+        return pankki.tilisiirto(nimi, viite, tiliNumero, kaupanTili, summa);
+    }
+
+}
---
+++ viikko2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/VarastoIo.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ohtu.verkkokauppa;
+
+/**
+ *
+ * @author anttihalmetoja
+ */
+public interface VarastoIo {
+
+    Tuote haeTuote(int id);
+
+    void otaVarastosta(Tuote t);
+
+    void palautaVarastoon(Tuote t);
+
+    int saldo(int id);
+    
+}
